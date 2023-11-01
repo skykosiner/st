5aefa3a27620d619db566f089e5062e4d6a3636c
Sky Kosiner <sky@skykosiner.co.uk>
2023-11-01 01:11:36 +0000

    commit?

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "mono:pixelsize=20:antialias=true:autohint=true";
 static char *font2[] = { "NotoColorEmoji:pixelsize=10:antialias=true:autohint=true" };
 static int borderpx = 2;
 
@@ -112,31 +112,32 @@ float alphaOffset = 0.0;
 float alphaUnfocus;
 
 /* Terminal colors (16 first used in escape sequence) */
-static const char *colorname[] = {
-	"#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
-	"#cc241d",
-	"#98971a",
-	"#d79921",
-	"#458588",
-	"#b16286",
-	"#689d6a",
-	"#a89984",
-	"#928374",
-	"#fb4934",
-	"#b8bb26",
-	"#fabd2f",
-	"#83a598",
-	"#d3869b",
-	"#8ec07c",
-	"#ebdbb2",
-	[255] = 0,
+// static const char *colorname[] = {
+	// "#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
+	// "#cc241d",
+	// "#98971a",
+	// "#d79921",
+	// "#458588",
+	// "#b16286",
+	// "#689d6a",
+	// "#a89984",
+	// "#928374",
+	// "#fb4934",
+	// "#b8bb26",
+	// "#fabd2f",
+	// "#83a598",
+	// "#d3869b",
+	// "#8ec07c",
+	// "#ebdbb2",
+	// [255] = 0,
 	/* more colors can be added after 255 to use with DefaultXX */
-	"#add8e6", /* 256 -> cursor */
-	"#555555", /* 257 -> rev cursor*/
-	"#282828", /* 258 -> bg */
-	"#ebdbb2", /* 259 -> fg */
-};
+	// "#add8e6", /* 256 -> cursor */
+	// "#555555", /* 257 -> rev cursor*/
+	// "#010101", /* 258 -> bg */
+	// "#ebdbb2", /* 259 -> fg */
+// };
 
+#include "/home/sky/.cache/wal/colors-wal-st.h"
 
 /*
  * Default colors (colorname index)
@@ -162,7 +163,7 @@ static unsigned int cursorshape = 2;
  */
 
 static unsigned int cols = 80;
-static unsigned int rows = 24;
+static unsigned int rows = 22;
 
 /*
  * Default colour and shape of the mouse cursor
@@ -239,8 +240,8 @@ static MouseShortcut mshortcuts[] = {
 };
 
 /* Internal keyboard shortcuts. */
-#define MODKEY Mod1Mask
-#define TERMMOD (Mod1Mask|ShiftMask)
+#define MODKEY Mod4Mask
+#define TERMMOD (ControlMask|ShiftMask)
 
 static char *openurlcmd[] = { "/bin/sh", "-c", "st-urlhandler -o", "externalpipe", NULL };
 static char *copyurlcmd[] = { "/bin/sh", "-c", "st-urlhandler -c", "externalpipe", NULL };
@@ -272,17 +273,17 @@ static Shortcut shortcuts[] = {
 	{ MODKEY,               XK_Down,        kscrolldown,    {.i =  1} },
 	{ MODKEY,               XK_u,           kscrollup,      {.i = -1} },
 	{ MODKEY,               XK_d,           kscrolldown,    {.i = -1} },
-	{ MODKEY,		XK_s,		changealpha,	{.f = -0.05} },
-	{ MODKEY,		XK_a,		changealpha,	{.f = +0.05} },
+	{ TERMMOD,		XK_S,		changealpha,	{.f = -0.05} },
+	{ TERMMOD,		XK_A,		changealpha,	{.f = +0.05} },
 	{ TERMMOD,              XK_Up,          zoom,           {.f = +1} },
 	{ TERMMOD,              XK_Down,        zoom,           {.f = -1} },
 	{ TERMMOD,              XK_K,           zoom,           {.f = +1} },
 	{ TERMMOD,              XK_J,           zoom,           {.f = -1} },
 	{ TERMMOD,              XK_U,           zoom,           {.f = +2} },
 	{ TERMMOD,              XK_D,           zoom,           {.f = -2} },
-	{ MODKEY,               XK_l,           externalpipe,   {.v = openurlcmd } },
-	{ MODKEY,               XK_y,           externalpipe,   {.v = copyurlcmd } },
-	{ MODKEY,               XK_o,           externalpipe,   {.v = copyoutput } },
+	{ TERMMOD,               XK_L,           externalpipe,   {.v = openurlcmd } },
+	{ TERMMOD,               XK_Y,           externalpipe,   {.v = copyurlcmd } },
+	{ TERMMOD,               XK_O,           externalpipe,   {.v = copyoutput } },
 };
 
 /*

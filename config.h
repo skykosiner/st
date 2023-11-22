855b4f783aaf31e70c548360b8320a63459438d1
Sky Kosiner <sky@skykosiner.co.uk>
2023-11-22 14:31:20 +0000

    Better emoji stuff

--- config.h
+++ config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "FiraCodeNerdFontMono:pixelsize=25:antialias=true:autohint=true";
-static char *font2[] = { "NotoColorEmoji:pixelsize=15:antialias=true:autohint=true" };
+static char *font = "FiraCodeNerdFontMono:pixelsize=25";
+static char *font2[] = { "Noto Color Emoji:pixelsize=22" };
 static int borderpx = 2;
 
 /*
